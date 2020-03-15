9a5c0eb4cd0bf9b9819ca6f316ed546b6e874c9e
piggrief <zhuzhibeishang@126.com>
2020-03-15 17:53:52 +0800

    tf2.1 Learning:dropout_test

--- tf2.0_exercise/fashion_minist_test.py
+++ tf2.0_exercise/fashion_minist_test.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def softmax_model(if_label_one_hot):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # 扁平层
     model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))  # dropout层为了抑制过拟合 rate=0.5代表每次随机丢弃掉50%神经元
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))  # dropout层为了抑制过拟合 rate=0.5代表每次随机丢弃掉50%神经元
     model.add(tf.keras.layers.Dense(10, activation='softmax'))  # softmax层
     if not if_label_one_hot:
-        model.compile(optimizer='adam',
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                       loss='sparse_categorical_crossentropy',  # 分类标签用数字编码时使用sparse_categorical_crossentropy
                       metrics=['acc']
                       )
@@ -22,7 +26,6 @@ def softmax_model(if_label_one_hot):
 
 
 if __name__ == '__main__':
-
     fm_data_sets = tf.keras.datasets.fashion_mnist.load_data()
     train_image = fm_data_sets[0][0]
     train_label = fm_data_sets[0][1]
@@ -39,8 +42,19 @@ if __name__ == '__main__':
 
     model = softmax_model(True)
 
-    model.fit(train_image, train_label_one_hot, epochs=5)
+    his = model.fit(train_image, train_label_one_hot, epochs=5,
+                    validation_data=(test_image, test_label_one_hot))  # 在测试集上验证
 
+    # 绘制训练集和测试集上的loss和acc
+    plt.plot(his.epoch, his.history.get('loss'), label='train_loss')
+    plt.plot(his.epoch, his.history.get('val_loss'), label='valid_loss')
+    plt.legend()
+    plt.figure()
+    plt.plot(his.epoch, his.history.get('acc'), label='train_acc')
+    plt.plot(his.epoch, his.history.get('val_acc'), label='valid_acc')
+    plt.legend()
+
+    plt.show()
     print(model.evaluate(test_image, test_label_one_hot))  # 评价模型
 
     input()
